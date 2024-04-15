d3510a543822d618dc04b3bcad45f9159df2931c
vivek-panchal <vivek.nsut@gmail.com>
2024-04-15 11:51:30 +0530

    Post a job

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            hostname: "g5dx4zowmemhzky1.public.blob.vercel-storage.com",
+          },
+        ],
+    },
+};
 
 export default nextConfig;
--- src/app/jobs/new/actions.ts
+++ src/app/jobs/new/actions.ts
@@ -54,6 +54,7 @@ export async function createJobPosting(formData: FormData) {
       applicationUrl: applicationUrl?.trim(),
       description: description?.trim(),
       salary: parseInt(salary),
+      approved : true
     },
   });
 
