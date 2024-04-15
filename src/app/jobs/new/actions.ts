6dd7070307eacb75382fe9efba76492accb4b29d
vivek-panchal <vivek.nsut@gmail.com>
2024-04-15 13:15:45 +0530

    Impliment job details page

--- src/app/jobs/new/actions.ts
+++ src/app/jobs/new/actions.ts
@@ -54,7 +54,6 @@ export async function createJobPosting(formData: FormData) {
       applicationUrl: applicationUrl?.trim(),
       description: description?.trim(),
       salary: parseInt(salary),
-      approved : true
     },
   });
 
